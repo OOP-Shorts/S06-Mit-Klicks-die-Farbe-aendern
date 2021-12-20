fa3b9e52a1bfdc57d8301dbed5b65cb652b2347c
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-20 20:26:00 +0100

    Add solution

--- src/S06ClickableCircle.java
+++ src/S06ClickableCircle.java
@@ -1,19 +1,31 @@
 import de.ur.mi.oop.app.GraphicsApp;
-import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
+import de.ur.mi.oop.events.MouseReleasedEvent;
+import de.ur.mi.oop.graphics.Circle;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
 public class S06ClickableCircle extends GraphicsApp {
 
-   
+    private Circle circle;
+
     @Override
     public void initialize() {
         setCanvasSize(500, 500);
+        circle = new Circle(getWidth() / 2, getHeight() / 2, 50, Colors.RED);
     }
 
     @Override
     public void draw() {
         drawBackground(Colors.BLACK);
+        circle.draw();
+    }
+
+    @Override
+    public void onMouseReleased(MouseReleasedEvent event) {
+        boolean circleWasClicked = circle.hitTest(event.getXPos(), event.getYPos());
+        if (circleWasClicked) {
+            circle.setColor(Colors.getRandomColor());
+        }
     }
 
     public static void main(String[] args) {
